e73bac3e74bba8ca4fa80a99943391c55ea1c3ba
許哲瑜 <jerryhsu1218@gmail.com>
2020-06-01 18:19:38 +0800

    1

---
+++ action.php
@@ -0,0 +1,20 @@
+<a href = "index.php"> Go Query Interface</a> <p>
+<?php
+	if(isset($_POST['MyHead'])) {
+		$MyHead=$_POST["MyHead"];
+	
+		$dbhost = '127.0.0.1';
+		$dbuser = 'hj';
+		$dbpass = 'test1234';
+		$dbname = 'testdb';
+		$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
+		mysqli_query($conn, "SET NAMES 'utf8'");
+		mysqli_select_db($conn, $dbname);
+		$sql = "SELECT description FROM people where name LIKE \"".$MyHead."%\";";
+		$result = mysqli_query($conn, $sql) or die('MySQL query error');
+		while($row = mysqli_fetch_array($result)){
+			echo $row['description']."<p>";
+		}
+	}
+?>
+© 2020 GitHub, Inc.
---
+++ sign.php
@@ -0,0 +1,19 @@
+<div class="container">
+        <form action="/action_page.php">
+            <label for="name">您的姓名:</label>
+            <input type="text" id="name" name="name" required><br><br>
+            <label for="phone">電話號碼:</label>
+            <input type="tel" id="phone" name="phone" placeholder="0912345678" pattern="[0-9]{10}"
+                required><br>
+            <small>Format: 1234567890</small><br><br>
+            <label for="adrs">您的地址:</label>
+            <input type="text" id="adrs" name="adrs" required><br><br>
+            <label for="email">電子信箱:</label>
+            <input type="email" id="email" name="email" required><br><br>
+            <label for="pwd">您的密碼:</label>
+            <input type="password" id="pwd" name="pwd" minlength="8" required><br><br>
+            <input type="submit">
+
+        </form>
+    </div>
+</body>
